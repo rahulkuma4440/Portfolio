b9ba0c163b9b910226e1e2c85c6a919449914403
rahulkuma4440 <103595655+rahulkuma4440@users.noreply.github.com>
2023-07-11 23:05:40 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react'
+// // import { BrowserRouter , Routes , Route } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <>
+//     <header className="header">
+//       <Link to="#" className="logo">Portfolio</Link>
+//       <nav className="navbar">
+//         <Link to="#" className="Home">Home</Link>
+//         <Link to="#" className="Home">About</Link>
+//         <Link to="#" className="Home">Skill</Link>
+//         <Link to="#" className="Home">Portfolio</Link>
+//         <Link to="#" className="Home">Contact</Link>
+//       </nav>
+//     </header>
+//     <section className="home">
+//       <div className='home-content'>
+//         <h3>Hello, this is me</h3>
+//         <h1>Rahul Kumar</h1>
+//         <p>I am a Mathematics and Computing student at IIT Guwahati
+//           <br /> I have more interest in the backend of any website
+//         </p>
+//       </div>
+//     </section>
+//     </>
 
-export default App;
+//   )
+// }
+
+// export default App
